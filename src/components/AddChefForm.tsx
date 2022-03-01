bd2438d84a74a0915a7bb2dd022e8ea869c8861c
Eunhee Park <eunieuni.corn@gmail.com>
2022-03-02 04:29:31 +0900

    fix AddChefForm

--- src/components/AddChefForm.tsx
+++ src/components/AddChefForm.tsx
@@ -56,30 +56,74 @@ export default function AddChefForm(props: AddChefProps): JSX.Element {
     );
 
     return (
-        <form onSubmit={handleSubmit(onClickAddChef)} className="text-center">
-            {_.map(daysOfWeek, (item, index) => {
-                return (
-                    // TODO: combobox로 바꾸기
-                    <div key={index} className="flex-1 mb-2">
-                        <section className={'section'} key={index}>
+        <div className="p-3">
+            {/* section title */}
+            <div className="mb-6">
+                <div className="relative h-6 mb-1">
+                    <div className="
+                        absolute -top-2 -left-4 -rotate-12
+                        w-8 h-4 rounded-[32px/16px] bg-primary
+                        flex justify-center items-center
+                    ">
+                        <span className="text-[10px] text-white font-bold leading-none">
+                            NOT
+                        </span>
+                    </div>
+                    <h3 className="absolute">Chefs of The Week</h3>
+                </div>
+                <p className="text-sm text-gray-700">
+                    일주일의 주문자를 등록해주세요.
+                </p>
+            </div>
+            <form onSubmit={handleSubmit(onClickAddChef)}>
+                {_.map(daysOfWeek, (item, index) => {
+                    return (
+                        // TODO: combobox로 바꾸기
+                        <div key={index} className="input mb-2">
                             <input
-                                className=""
                                 id={`add-chef-${index}`}
                                 type="text"
                                 placeholder={item}
                                 {...register(`addChef.${index}`)}
                             />
-                        </section>
-                    </div>
-                );
-            })}
-            <button
-                className="btn btn-ghost"
-                type="submit"
-                disabled={isSubmitting}
-            >
-                {chefs.length > 0 ? '주문자 변경' : '주문자 추가'}
-            </button>
-        </form>
+                        </div>
+                    );
+                })}
+            </form>
+            <div className="flex justify-center gap-2">
+                {chefs.length > 0 &&
+                    <button
+                        type="button"
+                        className="btn btn-ghost"
+                    >
+                        주문자 변경
+                    </button>
+                }
+                {chefs.length === 0 &&
+                    <button
+                        type="submit"
+                        className="btn"
+                        disabled={isSubmitting}
+                    >
+                        주문자 등록
+                    </button>
+                }
+                {/* @hyunjin TODO: 주문자 변경 버튼을 누르면 [취소/등록] 버튼으로 바뀌도록 */}
+                <button
+                    type="button"
+                    className="btn btn-ghost"
+                    disabled={isSubmitting}
+                >
+                    취소
+                </button>
+                <button
+                    type="submit"
+                    className="btn"
+                    disabled={isSubmitting}
+                >
+                    등록
+                </button>
+            </div>
+        </div>
     );
 }

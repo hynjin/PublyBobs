769d0ef81530bd029ea767b68134b3f7d88b9f5d
Eunhee Park <eunhee@publy.co>
2022-03-09 23:24:28 +0900

    fix order form (restaurant + menu)

--- src/components/AddMenuForm.tsx
+++ src/components/AddMenuForm.tsx
@@ -5,6 +5,7 @@ import {
     useFieldArray,
 } from 'react-hook-form';
 import _ from 'lodash';
+import { XIcon } from '@heroicons/react/outline';
 
 type AddMenuFormProps = {
     control: Control<FieldValues, any>;
@@ -12,7 +13,7 @@ type AddMenuFormProps = {
 };
 
 export default function AddMenuForm(props: AddMenuFormProps) {
-    const { control, index } = props;
+    const { control, index, idx } = props;
 
     const emptyMenu = {
         name: '',
@@ -28,55 +29,83 @@ export default function AddMenuForm(props: AddMenuFormProps) {
         <div key={`order-${index}`}>
             {fields.map((field, idx) => {
                 return (
-                    <div key={field.id}>
-                        <div className="mb-6 flex">
+                    idx === fields.length - 1 ? (
+                        <div key={field.id} className="mt-8 flex flex-col">
                             <Controller
                                 control={control}
                                 name={`order.${index}.menu.${idx}.name`}
                                 render={({ field }) => (
                                     <input
-                                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                        className="input mb-2"
                                         type="text"
                                         placeholder="새로운 메뉴"
                                         {...field}
                                     />
                                 )}
                             />
-                            <button
-                                type="button"
-                                className="btn"
-                                onClick={() => {
-                                    remove(idx);
-                                }}
-                            >
-                                메뉴 삭제
-                            </button>
-                        </div>
-                        <div className="mb-6">
                             <Controller
                                 control={control}
                                 name={`order.${index}.menu.${idx}.description`}
                                 render={({ field }) => (
                                     <input
-                                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                        className="input mb-2"
                                         type="text"
                                         placeholder="메모를 추가할 수 있어요. (40자)"
                                         {...field}
                                     />
                                 )}
                             />
+                            {/* TODO: 식당이름이 비어있을 때에는 메뉴 추가가 되지 않아야 함 (현재 빈칸이 입력됨) */}
+                            <button
+                                type="button"
+                                className="btn btn-light"
+                                onClick={() => append(emptyMenu)}
+                            >
+                                메뉴 추가
+                            </button>
                         </div>
-                    </div>
+                    ) : (
+                        <div key={field.id} className="mb-2 border flex">
+                            <div className="p-3 flex-1">
+                                <Controller
+                                    control={control}
+                                    name={`order.${index}.menu.${idx}.name`}
+                                    render={({ field }) => (
+                                        <input
+                                            className="text-sm text-gray-900 p-0 bg-white"
+                                            type="text"
+                                            disabled
+                                            {...field}
+                                        />
+                                    )}
+                                />
+                                {/* TODO: input 값이 있을 때에만 나오도록 */}
+                                <Controller
+                                    control={control}
+                                    name={`order.${index}.menu.${idx}.description`}
+                                    render={({ field }) => (
+                                        <input
+                                            className="text-xs text-gray-700 p-0 bg-white"
+                                            type="text"
+                                            disabled
+                                            {...field}
+                                        />
+                                    )}
+                                />
+                            </div>
+                            <button
+                                type="button"
+                                className="flex-none p-3"
+                                onClick={() => {
+                                    remove(idx);
+                                }}
+                            >
+                                <XIcon className="w-5 h-5 text-gray-700" />
+                            </button>
+                        </div>
+                    )
                 );
             })}
-
-            <button
-                type="button"
-                className="btn"
-                onClick={() => append(emptyMenu)}
-            >
-                메뉴 추가
-            </button>
         </div>
     );
 }
--- src/components/AddOrderForm.tsx
+++ src/components/AddOrderForm.tsx
@@ -119,11 +119,11 @@ export default function AddChefForm(props: AddOrderProps): JSX.Element {
                     </button>
                 </div>
             </div>
-            <div className="px-3 grid grid-cols-3 gap-6">
+            <div className="py-3 grid grid-cols-3 divide-x divide-gray-100">
                 {fields.map((field, index) => {
                     return (
                         <div key={field.id}>
-                            <section className={'section'} key={field.id}>
+                            <section className="section p-3" key={field.id}>
                                 <AddRestaurantForm
                                     control={control}
                                     index={index}
@@ -132,7 +132,7 @@ export default function AddChefForm(props: AddOrderProps): JSX.Element {
                                 <AddMenuForm control={control} index={index} />
                                 <button
                                     type="button"
-                                    className="btn"
+                                    className="mt-8 p-3"
                                     onClick={() => remove(index)}
                                 >
                                     식당 삭제
